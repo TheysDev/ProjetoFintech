aee68799710cf413d1736020bef8b7e1321408dd
Matheus Borges <theys.dev@gmail.com>
2024-11-13 22:25:53 -0300

    Login e cadastro atualizados

--- src/main/java/br/com/fiap/fintech/controller/LoginServlet.java
+++ src/main/java/br/com/fiap/fintech/controller/LoginServlet.java
@@ -33,7 +33,7 @@ public class LoginServlet extends HttpServlet {
             if(dao.validarLogin(usuario)){
                 HttpSession session = req.getSession();
                 session.setAttribute("usuario", usuario);
-                req.getRequestDispatcher("/index.jsp").forward(req, resp);
+                req.getRequestDispatcher("/dashboard.jsp").forward(req, resp);
             } else {
                 req.setAttribute("erro", "Usuário ou senha incorretos");
                 req.getRequestDispatcher("/login.jsp").forward(req, resp);
